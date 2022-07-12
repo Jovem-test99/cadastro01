c8f8cda42857e4027f06cda7f8008fdc638c79fe
Jovem-test99 <109122245+Jovem-test99@users.noreply.github.com>
2022-07-12 18:15:07 -0300

    função do input-email

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ function cadastrar() {
     var ns = window.document.querySelector('input#name')
     var nome = (ns.value)
     var idade = Number(anos.value)
+    var email = window.document.querySelector('input#email')
+    var codemail = (email.value)
 
     
     if (idade < 18) {
         bmvd.innerHTML = `Olá, ${nome}!<br> Não conseguimos concluir o seu cadastro pois você não tem a idade minima necessária`
     } else if (idade >= 18) {
-        bmvd.innerHTML = `Olá, ${nome}! <br> O seu cadastro já foi concluído, em breve você irá receber um email com o código de segurança da sua conta`
+        bmvd.innerHTML = `<p>Olá, ${nome}! <br> O seu cadastro já foi concluído, em breve você irá receber o código de segurança da sua conta no email <span>${codemail}</span></p>`
     }
     }
